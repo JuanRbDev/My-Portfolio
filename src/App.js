cd4a5d4944144d749aae96b5d4f08e1d4a505250
JuanRbDev <juanrbdev.rodriguezdev@gmail.com>
2026-02-01 11:51:18 -0500

    config gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <h1>Hola Ariana
+
+      </h1>
+
+      <svg viewBox="0 0 100 100">
+        <path
+          class="heart"
+          d="
+        M50 85
+        C20 60, 5 45, 25 25
+        C40 10, 50 25, 50 25
+        C50 25, 60 10, 75 25
+        C95 45, 80 60, 50 85
+      "
+        />
+      </svg>
+
+      <br />
+
+      <img src='https://usagif.com/wp-content/uploads/gifs/hamster-34.gif' />
     </div>
+
+
   );
 }
 
